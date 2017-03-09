21426f89856af49711eb442d92333854eaf932cc
mike stedman <github@ravuya.com>
2017-03-09 13:44:10 -0700

    did normal map conversion, the easiest part...

--- megascan.py
+++ megascan.py
@@ -1,6 +1,17 @@
 from PIL import Image
 import sys, os
 
+def do_normal_map(directoryPath, familyRoot):
+    # pkfg22_4K_Normal.jpg -> pkfg22_4K_n.tga
+    jpg_path = os.path.join(directoryPath, familyRoot + "Normal.jpg")
+    if not os.path.exists(jpg_path):
+        print 'Normal map file is missing: %s' % jpg_path
+        sys.exit(1)
+    
+    tga_path = familyRoot + "n.tga"
+    Image.open(jpg_path).save(tga_path)
+    print " -> Normal map at %s" % tga_path
+
 def get_family_root(directoryPath):
     # figure out who the albedo is
     albedo_path = next(f for f in os.listdir(directoryPath) if "albedo" in f.lower())
@@ -15,6 +26,7 @@ def handle_directory(directoryPath):
         sys.exit(1)
     family_root = get_family_root(directoryPath)
     print 'Handling family at %s' % family_root
+    do_normal_map(directoryPath, family_root)
 
 def printUsage():
     print 'Usage: %s [directories]' % (sys.argv[0])
